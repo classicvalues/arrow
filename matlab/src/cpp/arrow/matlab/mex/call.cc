d1d6ac26425ecff78e1f64fc5f4120593822204a
Fiona La <fionala@mathworks.com>
2022-04-07 10:41:42 -0400

    Modify call.cc to use MATLAB Data API for C++, prints first input argument string to console.

--- matlab/src/cpp/arrow/matlab/mex/call.cc
+++ matlab/src/cpp/arrow/matlab/mex/call.cc
@@ -14,15 +14,87 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
+// #include <mex.h>
+// #include "mex_util.h"
+// void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
+//   using namespace arrow::matlab::mex;
+//   checkNumArgs(nrhs);
+//   auto fcn = lookup_function(get_function_name(prhs[0]));
+//   fcn(nlhs, plhs, nrhs - 1, ++prhs);
+// }
 
-#include <mex.h>
+// ----------- C++ mex implementation -----------
+#include<string>
+// #include<memory>
 
-#include "mex_util.h"
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+// TODO: Import after conversion
+// #include "mex_util.h"
+// TODO: Can use to simplify code, may make dependencies harder to understand
+using namespace matlab::data;
+using matlab::mex::ArgumentList;
+class MexFunction : public matlab::mex::Function {
+private:
+    std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr;
+public:
+    /* Constructor for the class. */
+    MexFunction() {
+      matlabPtr = getEngine();
+    }
 
-void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
-  using namespace arrow::matlab::mex;
+    void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
+      // Print the function name
+      const CharArray function_name = inputs[0];
+      std::ostringstream stream;
+      stream<< "Function name: " << function_name.toAscii() << std::endl;
+      displayInMATLAB(std::move(stream));
 
-  checkNumArgs(nrhs);
-  auto fcn = lookup_function(get_function_name(prhs[0]));
-  fcn(nlhs, plhs, nrhs - 1, ++prhs);
-}
+      // // Get function pointer based on inputs from MATLAB
+      // auto fcn = lookup_function(get_function_name(inputs[0]));
+      // Get all inputs after the function name
+      // std::vector<matlab::data::TypedArray<matlab::data::Array>> inputArgs(inputs.begin() + 1, inputs.end());
+      // // Call function with input and output argument lists
+      // fcn(outputs, inputArgs)
+    }
+
+    /* Helper function to print a stream to the MATLAB command prompt. */
+    void displayInMATLAB(std::ostringstream stream) {
+      // https://uk.mathworks.com/help/matlab/matlab_external/displaying-output-in-matlab-command-window.html
+      ArrayFactory factory;
+      matlabPtr->feval(u"fprintf", 0, std::vector<Array>
+              ({ factory.createScalar(stream.str())}));
+    }
+
+    void checkArguments(ArgumentList outputs, ArgumentList inputs) {
+        // Get pointer to engine
+        std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+
+        // Get array factory
+        ArrayFactory factory;
+
+        // Check first input argument
+        if (inputs[0].getType() != ArrayType::CHAR ||
+            inputs[0].getNumberOfElements() != 1)
+        {
+            matlabPtr->feval(u"error",
+                0,
+                std::vector<Array>({ factory.createScalar("First input must be scalar double") }));
+        }
+
+        // Check second input argument
+        if (inputs[1].getType() != ArrayType::DOUBLE)
+        {
+            matlabPtr->feval(u"error",
+                0,
+                std::vector<Array>({ factory.createScalar("Input must be double array") }));
+        }
+        // Check number of outputs
+        if (outputs.size() > 1) 
+        {
+            matlabPtr->feval(u"error",
+                0,
+                std::vector<Array>({ factory.createScalar("Only one output is returned") }));
+        }
+    }
+};
